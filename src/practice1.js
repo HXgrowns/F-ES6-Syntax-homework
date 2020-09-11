dd93ec28d5e34c3b7c3c998d1f90c386f1e4d33d
HXgrowns <804156017@qq.com>
2020-09-11 19:44:42 +0800

    [huxiao]: finish practice2 and refact practice1

--- src/practice1.js
+++ src/practice1.js
@@ -3,10 +3,10 @@ const parseData = ({ data, column }) => {
 
     let output = [];
     data.map(it => {
-        let obj = {}
-        for (let index = 0; index < column.length; index++) {
+        let obj = {};
+        it.forEach((element,index) => {
             obj[column[index].name] = it[index];
-        }
+        });
         output.push(obj);
     })
     return output;
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject;
+const inject = (items, sections) => {
+    sections.forEach(({ content, index }, id) => {
+        items.splice(index + id, 0, content);
+    });
+    return items;
+}
 export { inject };
