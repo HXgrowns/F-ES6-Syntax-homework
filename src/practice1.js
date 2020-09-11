0d9d427a650a3c3c60cef08f1d93bf7e5ebcc375
HXgrowns <804156017@qq.com>
2020-09-11 19:41:02 +0800

    [huxiao]: finish practice1.js

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+
+const parseData = ({ data, column }) => {
+
+    let output = [];
+    data.map(it => {
+        let obj = {}
+        for (let index = 0; index < column.length; index++) {
+            obj[column[index].name] = it[index];
+        }
+        output.push(obj);
+    })
+    return output;
+}
 export { parseData };
