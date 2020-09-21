d12dfcb402af567979781bcd67ed9f1b879fcde8
kevindongzg <dongkevin94@gmail.com>
2020-09-21 23:00:24 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,10 +1,11 @@
 
 const parseData = ({ data, column }) => {
-
+    // TODO 使用map做数组与数组的转化，不用声明output，查查map的使用
     let output = [];
     data.map(it => {
         let obj = {};
         it.forEach((element,index) => {
+            // TODO it[index]不就是element吗？
             obj[column[index].name] = it[index];
         });
         output.push(obj);
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO 第二个参数不要取名id
     sections.forEach(({ content, index }, id) => {
         items.splice(index + id, 0, content);
     });
